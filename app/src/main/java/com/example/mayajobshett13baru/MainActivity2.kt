44dcf0da6d6b3475b2476ab1dfe031861c2d0baa
NorHidayah <salma@gmail.com>
2022-10-10 07:24:58 +0700

    first commit

---
+++ app/src/main/java/com/example/mayajobshett13baru/MainActivity2.kt
@@ -0,0 +1,36 @@
+package com.example.mayajobshett13baru
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var namaEdit: EditText
+    private lateinit var kelasEdit: EditText
+    private lateinit var nisEdit: EditText
+    private lateinit var submitBtn: Button
+
+    private val NAME_KEY: String = "nama"
+    private val KELAS_KEY: String = "kelas"
+    private val NIS_KEY: String = "nis"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        namaEdit = findViewById(R.id.namaEdit)
+        kelasEdit = findViewById(R.id.kelasEdit)
+        nisEdit = findViewById(R.id.NisEdit)
+        submitBtn = findViewById(R.id.submitBtn)
+
+        submitBtn.setOnClickListener {
+            intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra(NAME_KEY, namaEdit.text.toString())
+            intent.putExtra(KELAS_KEY, kelasEdit.text.toString())
+            intent.putExtra(NIS_KEY, nisEdit.text.toString())
+            startActivity(intent)
+        }
+    }
+}
